93f0629ac22e7bfa43053a5758e2e522cd5025fb
Tiago Sampaio <tiago@tiagosampaio.com>
2018-12-14 16:47:49 -0200

    First commit of the module.

---
+++ registration.php
@@ -0,0 +1,10 @@
+<?php
+/**
+ * @author Tiago Sampaio <tiago@tiagosampaio.com>
+ */
+
+\Magento\Framework\Component\ComponentRegistrar::register(
+    \Magento\Framework\Component\ComponentRegistrar::MODULE,
+    'Frenet_Shipping',
+    __DIR__
+);
