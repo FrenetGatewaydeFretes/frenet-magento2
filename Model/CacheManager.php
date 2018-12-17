4a6c111fbc5add552edf21b0c99c269d9b9ce986
Tiago Sampaio <tiago@tiagosampaio.com>
2018-12-17 20:14:15 -0200

    Creating a new cache type.

---
+++ Model/CacheManager.php
@@ -0,0 +1,190 @@
+<?php
+
+declare(strict_types = 1);
+
+namespace Frenet\Shipping\Model;
+
+use Magento\Framework\Serialize\SerializerInterface;
+use Magento\Quote\Model\Quote\Address\RateRequest;
+
+/**
+ * Class CacheManager
+ *
+ * @package Frenet\Shipping\Model
+ */
+class CacheManager
+{
+    /**
+     * @var \Magento\Framework\App\Cache\StateInterface
+     */
+    private $cacheState;
+    
+    /**
+     * @var \Magento\Framework\App\CacheInterface
+     */
+    private $cache;
+    
+    /**
+     * @var SerializerInterface
+     */
+    private $serializer;
+    
+    /**
+     * @var Config
+     */
+    private $config;
+    
+    /**
+     * @var \Frenet\Shipping\Api\QuoteItemValidator
+     */
+    private $quoteItemValidator;
+    
+    public function __construct(
+        \Magento\Framework\Serialize\SerializerInterface $serializer,
+        \Magento\Framework\App\Cache\StateInterface $cacheState,
+        \Magento\Framework\App\CacheInterface $cache,
+        \Frenet\Shipping\Api\QuoteItemValidator $quoteItemValidator,
+        Config $config
+    ) {
+        $this->serializer = $serializer;
+        $this->cacheState = $cacheState;
+        $this->cache = $cache;
+        $this->config = $config;
+        $this->quoteItemValidator = $quoteItemValidator;
+    }
+    
+    /**
+     * @param RateRequest $request
+     *
+     * @return bool
+     */
+    public function load(RateRequest $request)
+    {
+        if (!$this->isCacheEnabled()) {
+            return false;
+        }
+        
+        $data = $this->cache->load($this->generateCacheKey($request));
+        
+        if ($data) {
+            $data = $this->prepareAfterLoading($data);
+        }
+        
+        return $data;
+    }
+    
+    
+    public function save(array $services, RateRequest $request)
+    {
+        $identifier = $this->generateCacheKey($request);
+        $lifetime = null;
+        $tags = [
+            \Frenet\Shipping\Cache\Type\Frenet::CACHE_TAG
+        ];
+        
+        return $this->cache->save($this->prepareBeforeSaving($services), $identifier, $tags, $lifetime);
+    }
+    
+    /**
+     * @param $data
+     *
+     * @return array
+     */
+    private function prepareAfterLoading($data)
+    {
+        $newData = [];
+    
+        $services = $this->serializer->unserialize($data);
+        
+        /** @var array $service */
+        foreach ($services as $service) {
+            $newData[] = ($this->createServiceInstance())->setData($service);
+        }
+        
+        return $newData;
+    }
+    
+    /**
+     * @param array $services
+     *
+     * @return bool|string
+     */
+    private function prepareBeforeSaving(array $services)
+    {
+        $newData = [];
+        
+        /** @var \Frenet\ObjectType\Entity\Shipping\QuoteInterface $services */
+        foreach ($services as $service) {
+            $newData[] = $service->getData();
+        }
+    
+        return $this->serializer->serialize($newData);
+    }
+    
+    /**
+     * @return string
+     */
+    private function generateCacheKey(RateRequest $request)
+    {
+        $destPostcode = $request->getDestPostcode();
+        $origPostcode = $this->config->getOriginPostcode();
+        $items = [];
+        
+        /** @var \Magento\Quote\Model\Quote\Item $item */
+        foreach ($request->getAllItems() as $item) {
+            if (!$this->quoteItemValidator->validate($item)) {
+                continue;
+            }
+            
+            $productId = (int) $item->getProductId();
+            
+            if ($item->getParentItem()) {
+                $productId = $item->getParentItem()->getProductId() . '-' . $productId;
+            }
+            
+            $qty = (float) $item->getQty();
+            
+            $items[$productId] = $qty;
+        }
+        
+        ksort($items);
+        
+        $cacheKey = $this->serializer->serialize([
+            $this->normalizePostcode($origPostcode),
+            $this->normalizePostcode($destPostcode),
+            $items
+        ]);
+        
+        return $cacheKey;
+    }
+    
+    /**
+     * @return bool
+     */
+    private function isCacheEnabled()
+    {
+        return (bool) $this->cacheState->isEnabled(\Frenet\Shipping\Cache\Type\Frenet::TYPE_IDENTIFIER);
+    }
+    
+    /**
+     * @return \Frenet\ObjectType\Entity\Shipping\Quote\Service
+     */
+    private function createServiceInstance()
+    {
+        return new \Frenet\ObjectType\Entity\Shipping\Quote\Service(
+            new \Frenet\Framework\Data\Serializer()
+        );
+    }
+    
+    /**
+     * @param string $postcode
+     *
+     * @return string|string[]|null
+     */
+    private function normalizePostcode($postcode)
+    {
+        $postcode = preg_replace('/[^0-9]/', null, $postcode);
+        $postcode = str_pad($postcode, 8, '0', STR_PAD_LEFT);
+        return $postcode;
+    }
+}
