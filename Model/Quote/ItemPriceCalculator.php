fa5f631931393a5f48a34288e7df10f4cd38c2db
Tiago Sampaio <tiago@tiagosampaio.com>
2020-04-02 17:17:22 -0300

    Changes:
    - Removed unnecessary code.

--- Model/Quote/ItemPriceCalculator.php
+++ Model/Quote/ItemPriceCalculator.php
@@ -40,7 +40,6 @@ class ItemPriceCalculator
     public function getPrice(QuoteItem $item)
     {
         return $this->priceCalculatorFactory->create($item)->getPrice($item);
-//        return $this->getRealItem($item)->getPrice();
     }
 
     /**
@@ -51,43 +50,5 @@ class ItemPriceCalculator
     public function getFinalPrice(QuoteItem $item)
     {
         return $this->priceCalculatorFactory->create($item)->getFinalPrice($item);
-        /** @var QuoteItem $realItem */
-//        $realItem = $this->getRealItem($item);
-//        return $realItem->getRowTotal() / $this->itemQuantityCalculator->calculate($realItem);
-    }
-
-    /**
-     * @param QuoteItem $item
-     *
-     * @return QuoteItem
-     */
-    private function getRealItem(QuoteItem $item)
-    {
-        $type = $item->getProductType();
-
-        if ($item->getParentItemId()) {
-            $type = $item->getParentItem()->getProductType();
-        }
-
-        switch ($type) {
-            case \Magento\ConfigurableProduct\Model\Product\Type\Configurable::TYPE_CODE:
-                return $item->getParentItem();
-
-            case \Magento\GroupedProduct\Model\Product\Type\Grouped::TYPE_CODE:
-                /**
-                 * Product is Grouped.
-                 * @todo Validate if this approach is the correct one.
-                 */
-            case \Magento\Catalog\Model\Product\Type::TYPE_BUNDLE:
-                /**
-                 * Product is Bundle.
-                 * @todo Validate if this approach is the correct one.
-                 */
-            case \Magento\Catalog\Model\Product\Type::TYPE_VIRTUAL:
-            case \Magento\Downloadable\Model\Product\Type::TYPE_DOWNLOADABLE:
-            case \Magento\Catalog\Model\Product\Type::TYPE_SIMPLE:
-            default:
-                return $item;
-        }
     }
 }
