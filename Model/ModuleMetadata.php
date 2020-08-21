dba391afcd99ebbbc1247709d522fda543b073a0
Tiago Sampaio <tiago@tiagosampaio.com>
2020-08-20 21:04:08 -0300

    Added version information even when it's not installed via composer.

--- Model/ModuleMetadata.php
+++ Model/ModuleMetadata.php
@@ -17,6 +17,7 @@ namespace Frenet\Shipping\Model;
 use Magento\Framework\App\CacheInterface;
 use Magento\Framework\App\Config;
 use Magento\Framework\Composer\ComposerInformation;
+use Magento\Framework\Serialize\Serializer\Json;
 
 /**
  * Class ModuleMetadata
@@ -58,12 +59,19 @@ class ModuleMetadata
      */
     private $cache;
 
+    /**
+     * @var Json
+     */
+    private Json $serializer;
+
     public function __construct(
         ComposerInformation $composerInformation,
-        CacheInterface $cache
+        CacheInterface $cache,
+        Json $serializer
     ) {
         $this->composerInformation = $composerInformation;
         $this->cache = $cache;
+        $this->serializer = $serializer;
     }
 
     /**
@@ -109,7 +117,11 @@ class ModuleMetadata
         $package = $this->getPackage();
 
         if (isset($package['version'])) {
-            return $package['version'];
+            return $package['version'] . ' ' . __('(Installed Via Composer)');
+        }
+
+        if ($this->getLocalVersion()) {
+            return $this->getLocalVersion();
         }
 
         return __('Unknown Module Version');
@@ -143,5 +155,40 @@ class ModuleMetadata
         if (isset($packages[self::PACKAGE_NAME])) {
             $this->package = $packages[self::PACKAGE_NAME];
         }
+    }
+
+    /**
+     * @return string|null
+     */
+    private function getLocalVersion()
+    {
+        if ($this->version) {
+            return $this->version;
+        }
+
+        $metadata = $this->getLocalComposerInfo();
+        if (!isset($metadata['version'])) {
+            return null;
+        }
+
+        $this->version = $metadata['version'] . ' ' . __('(Installed in app/code)');
+        return $this->version;
+    }
+
+    /**
+     * @return array
+     */
+    private function getLocalComposerInfo()
+    {
+        $composerJson = dirname(__DIR__) . '/composer.json';
+
+        if (!file_exists($composerJson) && is_readable($composerJson)) {
+            return [];
+        }
+
+        $content = file_get_contents($composerJson);
+        $metadata = (array) $this->serializer->unserialize($content);
+
+        return $metadata;
     }
 }
